7e72e5976e80de0f999a9e53da4d748ab8747d98
tdefoggi <tim.defoggi@vcalc.com>
2013-09-07 16:46:34 -0400

    adding unlike to vote.php

--- server/vote.php
+++ server/vote.php
@@ -4,13 +4,26 @@ or die("could not connect");
 mysql_select_db("putpocket") or die ("No DB");
 
 $photoID = $_POST["id"];
+$unlike = $_POST["unlike"];
+$updateStatement = "";
 
 if ((is_null($photoID)) || (!is_numeric($photoID))) {
 	echo "Return Code: -1 <br />";
+	return;
+}
+
+if (is_null($unlike)) {
+	$updateStatement = "+ 1";
 } else {
-	
-	mysql_query("UPDATE photos SET likes = likes + 1 WHERE id = " . $photoID) or die(mysql_error());
+	if (!is_numeric($unlike)) {		
+	 	echo "Return Code: -2 <br />";
+	} else {
+		$updateStatement = "- 1";
+	}
 	
 }
+
+	mysql_query("UPDATE photos SET likes = likes " . $updateStatement . " WHERE id = " . $photoID) or die(mysql_error());
+	
 
 ?>
