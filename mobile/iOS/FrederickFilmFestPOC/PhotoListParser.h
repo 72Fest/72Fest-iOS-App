b5541d4d7b47f5f18bce8b23edc40c73d68c8e51
tdefoggi <tim.defoggi@vcalc.com>
2013-09-07 18:38:53 -0400

    Updated support for reading JSON vs XML - still debugging!

--- mobile/iOS/FrederickFilmFestPOC/PhotoListParser.h
+++ mobile/iOS/FrederickFilmFestPOC/PhotoListParser.h
@@ -9,6 +9,7 @@
 #import <Foundation/Foundation.h>
 #import "PhotoListParserProtocol.h"
 
+
 #define XML_TAG_ENTRIES @"entries"
 #define XML_TAG_IMAGE @"image"
 #define XML_TAG_ID @"id"
@@ -17,17 +18,12 @@
 #define XML_TAG_EVENT @"event"
 #define XML_TAG_APPROVED @"approved"
 
-@interface PhotoListParser : NSObject <NSXMLParserDelegate> {
+@interface PhotoListParser : NSObject {
     NSMutableArray *appData;
 }
 
 - (void)loadURL:(NSURL *)url;
 
-//protocol methods
-- (void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName attributes:(NSDictionary *)attributeDict;
-- (void)parser:(NSXMLParser *)parser didEndElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName;
-- (void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)string;
-- (void)parserDidEndDocument:(NSXMLParser *)parser;
 
 @property (nonatomic, retain) NSMutableArray *photoList;
 @property (nonatomic, assign) id delegate;
