8d7149933a9b0fd7a208dbd7c9636e1f52f21b92
Lonny Gomes <code@lonnygomes.com>
2013-09-12 00:17:32 -0400

    Fixed  issue where the web call to vote was wrapped in an uneeded thread - refactored the ConnectionInfo section for URL locations - added proper responses for voting results

--- mobile/iOS/FrederickFilmFestPOC/ConnectionInfo.h
+++ mobile/iOS/FrederickFilmFestPOC/ConnectionInfo.h
@@ -8,16 +8,19 @@
 
 #ifndef FrederickFilmFestPOC_ConnectionInfo_h
 #define FrederickFilmFestPOC_ConnectionInfo_h
+//URL Macros
+#define BASE_URL @"http://54.227.130.170/photoapp"
+#define GEN_URL(URL) [NSString stringWithFormat:@"%@/%@", BASE_URL, URL]
 
-#define PHOTO_LIST_URL_STR @"http://putpocket.com/photoapp/getImagesJs.php"
-#define UPLOAD_URL_STR  @"http://putpocket.com/photoapp/submit.php"
-#define VOTE_URL_STR  @"http://putpocket.com/photoapp/vote.php"
+//URL definitions
+#define COUNTDOWN_METADATA_URL GEN_URL(@"countDown.php")
+#define PHOTO_LIST_URL_STR GEN_URL(@"getImagesJs.php")
+#define UPLOAD_URL_STR  GEN_URL(@"submit.php")
+#define VOTE_URL_STR  GEN_URL(@"vote.php")
+
+//Other links
 #define EMAIL_URL_STR @"http://www.72fest.com/about/contact/"
 #define DESIGN_URL_STR @"http://www.jbokim.com"
 #define MAIN_URL_STR @"http://www.72fest.com"
 
-//UPLOAD_URL_STR = @"http://putpocket.com/imageuploadtest/picupload.php"
-//UPLOAD_URL_STR = @"http://192.168.1.7:8080/picupload.php"        
-//UPLOAD_URL_STR = @"http://lonnygomes.com/72fest/submit.php"
-//UPLOAD_URL_STR = @"http://www.72fest.com/72Photo/submit.php"
 #endif
--- mobile/iOS/FrederickFilmFestPOC/PhotoDetailViewController.h
+++ mobile/iOS/FrederickFilmFestPOC/PhotoDetailViewController.h
@@ -10,6 +10,9 @@
 #define VOTE_UP_ICON_IMG [UIImage imageNamed:@"voteUpIcon.png"]
 #define VOTE_DOWN_ICON_IMG [UIImage imageNamed:@"voteDownIcon.png"]
 
+#define VOTE_RESULT_STATUS_KEY @"status"
+#define VOTE_REULST_TOTALS_KEY @"votes"
+
 @interface PhotoDetailViewController : NIToolbarPhotoViewController <NIPhotoAlbumScrollViewDataSource, NIPagingScrollViewDelegate, NSURLConnectionDelegate>
 
 @property (nonatomic, strong) NSArray *photosList;
