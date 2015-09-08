184112b26f527eba86c86d808250fbdc20f6d1c6
Lonny Gomes <code@lonnygomes.com>
2015-09-07 22:51:24 -0400

    Added Sponsors web view to project

---
+++ mobile/iOS/FrederickFilmFestPOC/SponsorsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SponsorsViewController.h
+//  FrederickFilmFestPOC
+//
+//  Created by Carpe Lucem Media Group on 9/7/15.
+//
+//
+
+#import "GenericWebContentViewController.h"
+
+@interface SponsorsViewController : GenericWebContentViewController <UIWebViewDelegate>
+
+@end
